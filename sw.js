2c03e3cee7e3a3c63ffe8f4940d6aea8a96b7543
Jose Gonzalez Mato <44868118+joegon1@users.noreply.github.com>
2019-12-03 21:27:10 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+//Instalar el service worker
+self.addEventListener("install", evt => {console.log("El Service Worker se instaló")});
+
+//Activar el Service Worker.
+
+
+
+//Eventos Fetch (fetch request o pedido de busqueda).
