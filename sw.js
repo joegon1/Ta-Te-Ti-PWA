3cd8e5f16a1fc33e51b2802e83c3a69a42bc09c4
Jose Gonzalez Mato <44868118+joegon1@users.noreply.github.com>
2019-12-03 22:57:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
+const nombreCache="sitio-Cache";
+const elementos=["/","/.html","/css/estilo.css","/js/accion.js"];
+
+
+
 //Instalar el Service Worker
-self.addEventListener("install", evt => {console.log("El Service Worker se instaló");});
+self.addEventListener("install", evt => 
+	{
+		//console.log("El Service Worker se instaló");
+		evt.waitUntil(
+			caches.open(nombreCache).then ((cache)=>
+				{
+					console.log("Definimos el cache predeterminado");
+					cache.addAll(elementos);
+				})
+			);
+	});
 
 //Activar el Service Worker.
 self.addEventListener("activate", evt => {console.log("El Service Worker se activó");});
